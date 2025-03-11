432ec8f352075687addc662de6215b187b817689
l3miage-larbiy <larbi1992yasmine@gmail.com>
2025-03-11 14:59:32 +0100

    add users entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,19 +10,22 @@ import { User } from './modules/users/entity/users.entity';
 @Module({
   imports: [UsersModule,ConfigModule.forRoot({ isGlobal: true }), // Charge les variables .env
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD || '',
-      database: process.env.DB_NAME,
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'test',
+      database: 'proj-nest',
       entities: [User],
       autoLoadEntities: true, 
       synchronize: true, 
     }),
     
+    
   ],
+  
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
+
---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entity/users.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  findAll(): Promise<User[]> {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: string): Promise<User | null> {
+    return this.usersRepository.findOne({ where: { id: Number(id) } });
+  }
+
+  create(userData: Partial<User>): Promise<User> {
+    const newUser = this.usersRepository.create(userData);
+    return this.usersRepository.save(newUser);
+  }
+}
