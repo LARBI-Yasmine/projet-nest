064f070c87e3645ea41b3b67e80a1a189b685730
l3miage-larbiy <larbi1992yasmine@gmail.com>
2025-03-12 21:43:43 +0100

    add users controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,17 +1,24 @@
-import { Controller, Get, Param } from "@nestjs/common";
-import { UsersService } from "./users.service";
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
-@Controller("users")
+@Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) { }
+  constructor(private readonly usersService: UsersService) {}
 
-    @Get("/list")
-    getAll() {
-        return this.usersService.findAll();
-    }
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto): Promise<User> {
+    return this.usersService.create(createUserDto);
+  }
 
-    @Get("/:id")
-    getOne(@Param("id") id: string) {
-        return this.usersService.findOne(id);
-    }
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<User> {
+    return this.usersService.findOne(id);
+  }
 }
