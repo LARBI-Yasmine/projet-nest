ffe24f59f76da0610d5c1d685704da2d49c6741f
l3miage-larbiy <larbi1992yasmine@gmail.com>
2025-03-12 21:43:28 +0100

    add users service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,25 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 import { Repository } from 'typeorm';
-import { User } from './entity/users.entity';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { plainToInstance } from 'class-transformer';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
-    private usersRepository: Repository<User>,
+    private userRepository: Repository<User>,
   ) {}
 
-  findAll(): Promise<User[]> {
-    return this.usersRepository.find();
+  async findAll(): Promise<User[]> {
+    const users = await this.userRepository.find();
+    return plainToInstance(User, users);
   }
 
-  findOne(id: string): Promise<User | null> {
-    return this.usersRepository.findOne({ where: { id: Number(id) } });
+  async findOne(id: string): Promise<User> {
+    const user = await this.userRepository.findOneBy({ id });
+    return plainToInstance(User, user);
   }
 
-  create(userData: Partial<User>): Promise<User> {
-    const newUser = this.usersRepository.create(userData);
-    return this.usersRepository.save(newUser);
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const newUser = this.userRepository.create({ ...createUserDto });
+    const savedUser = await this.userRepository.save(newUser);
+    return plainToInstance(User, savedUser); //pour exclure le mdp de l'exposition
   }
 }
